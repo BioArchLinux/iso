174ca4e03f91742da5d967ef32edf7b2e77dd0b0
BioArchLinuxBot <bioarchlinux@malacology.net>
2023-03-21 18:03:32 +0000

    fix: make it suitable for systemd

--- gen_iso.sh
+++ gen_iso.sh
@@ -2,27 +2,27 @@
 # Run
 docker run  -itd --privileged --name bio bioarchlinux/bioarchlinux /bin/bash
 # System
-docker exec -it bio sh -c "ln -sf /usr/share/zoneinfo/GMT /etc/localtime"
-docker exec -it bio sh -c "pacman -Syu --noconfirm"
-docker exec -it bio sh -c "pacman -S archiso --noconfirm"
+docker exec -i bio sh -c "ln -sf /usr/share/zoneinfo/GMT /etc/localtime"
+docker exec -i bio sh -c "pacman -Syu --noconfirm"
+docker exec -i bio sh -c "pacman -S archiso --noconfirm"
 # Use mkarchiso
-docker cp bio/ bio:/root/
-docker exec -it bio sh -c "cd /root/bio &&  mkarchiso -C pacman.conf -v ."
-docker cp bio:/root/bio/out/bioarchlinux-$(date "+%Y.%m.%d")-x86_64.iso .
+docker cp /usr/share/lilac/iso/bio/ bio:/root/
+docker exec -i bio sh -c "cd /root/bio &&  mkarchiso -C pacman.conf -v ."
+docker cp bio:/root/bio/out/bioarchlinux-$(date "+%Y.%m.%d")-x86_64.iso /usr/share/lilac/iso/
 # Clean system
 docker stop bio
 docker rm bio
 docker rmi --force $(docker images -q)
 # GPG Sign
-gpg --output  bioarchlinux-$(date "+%Y.%m.%d")-x86_64.iso.sig --sign bioarchlinux-$(date "+%Y.%m.%d")-x86_64.iso
+gpg --output  /usr/share/lilac/iso/bioarchlinux-$(date "+%Y.%m.%d")-x86_64.iso.sig --sign /usr/share/lilac/iso/bioarchlinux-$(date "+%Y.%m.%d")-x86_64.iso
 # Sum sign
 for bin in $(pacman -Ql coreutils | grep 'sum$' | sed 's/\// /g' | awk '{print $4}')
 do
-	$bin bioarchlinux-$(date "+%Y.%m.%d")-x86_64.iso > bioarchlinux-$(date "+%Y.%m.%d")-x86_64.iso.$bin
+	$bin /usr/share/lilac/iso/bioarchlinux-$(date "+%Y.%m.%d")-x86_64.iso > /usr/share/lilac/iso/bioarchlinux-$(date "+%Y.%m.%d")-x86_64.iso.$bin
 done
 # Remove
 rm /usr/share/lilac/Repo/iso/*
 # Move
-mv bioarchlinux-$(date "+%Y.%m.%d")-x86_64.iso* /usr/share/lilac/Repo/iso
+mv /usr/share/lilac/iso/bioarchlinux-$(date "+%Y.%m.%d")-x86_64.iso* /usr/share/lilac/Repo/iso
 
 
